1b5fb85336254ee6b718d6ad5ade95b783f97118
BryantGouw <116624580+Bryantgouw@users.noreply.github.com>
2025-01-09 09:42:32 +0700

    Revised allow origins

--- service.py
+++ service.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  
+    allow_origins=["https://go-morse-website.vercel.app"],  
     allow_credentials=True,
     allow_methods=["*"],  
     allow_headers=["*"], 
